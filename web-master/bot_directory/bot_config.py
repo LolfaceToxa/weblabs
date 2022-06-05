b8e4a6a1afb648db80ec9a3ed6b83380bfd0bd6c
Anton <81695983+LolfaceToxa@users.noreply.github.com>
2022-06-05 22:06:05 +0300

    Update bot_config.py

--- web-master/bot_directory/bot_config.py
+++ web-master/bot_directory/bot_config.py
@@ -2,5 +2,5 @@ class BotConfig:
     """
     Class for keeping config data
     """
-    bot_token = '1797429681:AAHttgclx3NswSKnAP_JuN-CA7_jj8aszhY'
-    admins = list('1144246098')
+    bot_token = ''
+    admins = list('')
